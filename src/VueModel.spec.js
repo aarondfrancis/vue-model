b176f597a45c20216cb0e6ed63707284395f417b
Larry Battle <larry.battle.tech@gmail.com>
2017-10-29 11:31:14 -0500

    test: added assertions in unit tests

--- src/VueModel.spec.js
+++ src/VueModel.spec.js
@@ -1,28 +1,53 @@
+const _ = require('lodash');
 const Vue = require("vue");
+const VueModel = require('./VueModel');
 
 describe("VueModel", () => {
+
+    // This is need because Vue.use caches installed plugins
+    var fakeVueUse = (plugin, ...args) => {
+        plugin.install.apply(plugin, [Vue].concat(args));
+    };
+
     describe("Vue.models.register() should", () => {
+        beforeEach(() => {
+            VueModel.registry = {};
+            spyOn(VueModel, 'register').and.callThrough();
+        });
 
         it("register model(s)", () => {
-            const VueModel = require('./VueModel');
-            Vue.use(VueModel);
+            expect(VueModel.register).not.toHaveBeenCalled();
+
+            fakeVueUse(VueModel);
+
+            const baseRoute = '/test'
 
             Vue.models.register('users', {
                 http: {
-                    baseRoute: '/users',
-                },
+                    baseRoute
+                }
             });
+
+            expect(VueModel.register).toHaveBeenCalled();
+            expect(VueModel.registry.users).toBeTruthy();
+            expect(_.get(VueModel.registry.users, 'http.baseRoute')).toBe(baseRoute);
         });
 
         it("register model(s) when Vue.use(VueModel, options) is called", () => {
-            const VueModel = require('./VueModel');
-            Vue.use(VueModel, {
+            expect(VueModel.register).not.toHaveBeenCalled();
+            const baseRoute = '/test2'
+
+            fakeVueUse(VueModel, {
                 users: {
                     http: {
-                        baseRoute: '/users',
+                        baseRoute
                     }
                 }
             });
+
+            expect(VueModel.register).toHaveBeenCalled();
+            expect(VueModel.registry.users).toBeTruthy();
+            expect(_.get(VueModel.registry.users, 'http.baseRoute')).toBe(baseRoute);
         });
     });
 });
